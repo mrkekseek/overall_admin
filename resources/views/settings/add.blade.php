db33e6864ecf0554e46b9feff248144528263e54
Denys Domanskyi <manager@div-art.com>
2017-05-31 15:11:07 +0300

    Created "Settings" page

---
+++ resources/views/settings/add.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('breadcrumbs')
+<div class="breadcrumbs">
+    <h1>Add user</h1>
+
+    <ol class="breadcrumb">
+        <li>
+            <a href="/dashboard">Dashboard</a>
+        </li>
+        <li class="active">Settings Add user</li>
+    </ol>
+</div>
+@endsection
+
+@section('content')
+    Settings Add user
+@endsection
