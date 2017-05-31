b3cdee11f8b32859bcf6b232d08917eb7597434e
Denys Domanskyi <manager@div-art.com>
2017-05-31 17:39:05 +0300

    Created "Add new user" page

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,8 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\User;
 
 class SettingsController extends Controller
 {
-    
+    public function addNewUserPost($id = FALSE, $data = [])
+    {
+        $this->validate(request(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email_address' => 'required'
+        ]);
+
+        $user = new User;
+        $user->name = $data['first_name'].' '.$data['last_name'];
+        $user->username = $data['email_address'];
+        $user->email = $data['email_address'];
+        $user->address = $data['address'];
+        $user->city = $data['city'];
+        $user->country = $data['country'];
+        $user->save();
+    }
 }
--- resources/views/settings/add.blade.php
+++ resources/views/settings/add.blade.php
@@ -14,5 +14,81 @@
 @endsection
 
 @section('content')
-    Settings Add user
+<form role="form" action="/settings/addNewUser" method="post">
+	{{ csrf_field() }}
+	{{ method_field('POST') }}
+
+	<div class="row">
+	    <div class="col-md-6 col-xs-12">
+	        <div class="portlet light bordered">
+	            <div class="portlet-body form">
+	            	<h4>New user details</h4>
+
+	            	<div class="form-body">
+		                <div class="row">
+		                    <div class="col-sm-6 col-xs-12 form-group">
+		                        <label class="bold">First Name</label>
+		                        <input type="text" class="form-control" name="first_name" required="required" />
+		                        <span class="help-block">First Name is required</span>
+		                    </div>
+
+		                    <div class="col-sm-6 col-xs-12 form-group">
+		                        <label class="bold">Last Name</label>
+		                        <input type="text" class="form-control" name="last_name" required="required" />
+		                        <span class="help-block">Last Name is required</span>
+		                    </div>
+		                </div>
+
+		             	<div class="row">
+	                    	<div class="col-sm-6 col-xs-12 form-group">
+		                        <label class="bold">Email Address</label>
+		                        <input type="email" class="form-control" name="email_address" required="required" />
+		                        <span class="help-block">Email Address is required</span>
+		                    </div>
+
+		                    <div class="col-sm-6 col-xs-12 form-group">
+			                    <label class="bold">Role</label>
+		                        <select name="role" class="form-control">
+		                        	<option value="">Select a role from a list</option>
+		                            <option value="0">Administrator</option>
+		                            <option value="1">Employee</option>
+		                        </select>
+		               		 </div>
+		                </div>
+
+		                <div class="row">
+		                    <div class="col-sm-6 col-xs-12 form-group">
+		                        <label class="bold">Address</label>
+		                        <input type="text" class="form-control" name="address" required="required" />
+		                    </div>
+
+		                    <div class="col-sm-6 col-xs-12 form-group">
+		                        <label class="bold">City</label>
+		                        <input type="text" class="form-control" name="city" required="required" />
+		                    </div>
+		                </div>
+
+		                <div class="row">
+		                	<div class="col-sm-6 col-xs-12 form-group">
+		                        <label class="bold">Country</label>
+		                        <select name="country" class="form-control">
+		                            <option value="">Select country from a list</option>
+		                            <option value="US">United States</option>
+		                            <option value="FR">France</option>
+		                            <option value="UK">United Kingdom</option>
+		                            <option value="UA">Ukraine</option>
+		                            <option value="PL">Poland</option>
+		                        </select>
+		                    </div>
+		                </div>
+		            </div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
+
+	<div class="text-center">
+	    <button type="submit" class="btn green">Add new user</button>
+	</div>
+</form>
 @endsection
